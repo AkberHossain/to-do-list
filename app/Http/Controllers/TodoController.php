9e20f8d567cbc64032add3ff43c189f66eed4c4f
AkberHossain <crazzyremal199194@gmail.com>
2019-01-09 00:11:10 +0600

    completed

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
-    //
+    public function store()
+    {
+        $text = $_POST['text'];
+
+        Todo::create([
+
+            'todo' => $text ,
+            'user_id' => auth()->id() ,
+            'completed' => 0
+
+        ]);
+
+        //return redirect()->home();
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,63 @@
     <h1>TO DO LIST</h1>
     <br>
     
-    
-    <input type="text" name="todo" class="form-control" id="text-box">
+    <form method="POST" action=""  id="form-todo">
+        {{ csrf_field() }}
+        <input type="text" name="todo" class="form-control" id="text-box">
+        <br>
+        
+        <input type="submit" name="submit" class="btn btn-success btn-lg" id="add-btn" value="Add to your list" >
+    <form>
     <br>
-    
-    
-    
-    <a href="#" class="btn btn-success btn-lg" id="add-btn">Add to your list</a>
-    <br>
     <br>
 
+    @foreach($todos as $todo)
 
+        <div class="alert alert-primary" id="todo-box" role="alert">
+            <strong>{{ $todo->todo }}</strong>
+            <button type="button" class="btn btn-warning btn-sm float-left">
+                <span>Mark as Completed</span>
+            </button>
+            <button type="button" class="close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
 
+    @endforeach
 
-    <div class="alert alert-primary" id="todo-box" role="alert">
-        <strong>A simple primary alert with an example link. Give it a click if you like.</strong>
-        <button type="button" class="btn btn-warning btn-sm float-left">
-            <span>Mark as Completed</span>
-        </button>
-        <button type="button" class="close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    
     <script>
 
         $(document).ready(function(){
+
+            $("#form-todo").submit(function(){
+
+                $.ajax({
+                    url:{{route('todo.store')}} ,
+                    type:"POST" ,
+                    data: ,
+                    success:function(){
+                        alert("done");
+                    }
+                });
+
+            });
+
+            // $("form").submit(function(){
+
+            //     $.ajax({
+            //         url:"{{route('todo.store')}}" , 
+            //         data:$(this).serialize() ,
+            //         type:"GET" ,
+            //         success:function(data){
+
+            //             alert("ss");
+
+            //         }
+            //     });
+
+            // });
+
+
 
             // $("#add-btn").on("click" , function(){
 
